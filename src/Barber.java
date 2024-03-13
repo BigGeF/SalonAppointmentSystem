2b94dfef80b5e35a7295b5123ad8c0619bbf86e3
BigGeF <52731933+BigGeF@users.noreply.github.com>
2024-03-12 17:23:29 -0700

    Added new class called AppointmentManager.java
    In practical applications, using a String to represent time may not be accurate or easy to manage, especially when dealing with time and date calculations.

--- src/Barber.java
+++ src/Barber.java
@@ -1,36 +1,30 @@
-import java.util.HashSet;
-
 class Barber {
     private int barberId;
-    // here could be a external time management system
-    private HashSet<String> appointments;
+    private AppointmentManager appointmentManager;
 
     public Barber(int barberId) {
         this.barberId = barberId;
-        this.appointments = new HashSet<>();
+        this.appointmentManager = new AppointmentManager();
     }
 
     public boolean isAvailable(String time) {
-        return !appointments.contains(time);
+        return appointmentManager.isAvailable(time);
     }
 
-    public int getBarberId() {
-        System.out.println("BarberID is   " + barberId);
-        return barberId;
-    }
     public void bookAppointment(String time) {
         if (isAvailable(time)) {
-            appointments.add(time);
+            appointmentManager.bookAppointment(time);
         } else {
-            System.out.println("Sorry, this schedule is not available ");
+            System.out.println("Sorry, this schedule is not available");
         }
     }
 
     public void cancelAppointment(String time) {
-        if (appointments.contains(time)) {
-            appointments.remove(time);
-        } else {
-            System.out.println("No appointment for you to cancel at this schedule");
-        }
+        appointmentManager.cancelAppointment(time);
+    }
+
+    public int getBarberId() {
+        System.out.println("BarberID is " + barberId);
+        return barberId;
     }
 }
